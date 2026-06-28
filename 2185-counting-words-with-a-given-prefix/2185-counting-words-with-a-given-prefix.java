be255da10970f782ce64fa2ccbbde51c8b02810c
Uday More <149674624+Udaymore741@users.noreply.github.com>
2026-06-28 22:43:07 +0530

    Time: 0 ms (100%), Space: 44.5 MB (28.39%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -1,9 +1,5 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        
-    }
-}class Solution {
-    public int prefixCount(String[] words, String pref) {
         int count = 0;
 
         for (String word : words) {
